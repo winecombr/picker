e435653faaf179925e40d85659bb4b2802110b38
Sergio Henrique <54045191+Sergiohcp@users.noreply.github.com>
2023-07-06 11:30:18 -0300

    feat: fabric (#1)
    Co-authored-by: Sergio Henrique Carvalho Prudencio <sergioprudencio@C0344.local>

--- react-native.config.js
+++ react-native.config.js
@@ -15,6 +15,26 @@
  */
 'use strict';
 
+let supportsCodegenConfig = false;
+try {
+  const rnCliAndroidVersion = require('@react-native-community/cli-platform-android/package.json')
+    .version;
+  const [major] = rnCliAndroidVersion.split('.');
+  supportsCodegenConfig = major >= 9;
+} catch (e) {
+  // ignore
+}
+
+// this is a quick workaround for enabling custom codegen config in the fabric example app
+// the require above picks up the version of the cli-platform-android package from the root node_modules
+// instead of the one from the example app and doesn't correctly set the flag
+// this, along with the patch to the rncli allows to run the fabric example properly
+const codegenSwitch = '--enable-rnpicker-codegen';
+if (process.argv.includes(codegenSwitch)) {
+  process.argv = process.argv.filter((arg) => arg !== codegenSwitch);
+  supportsCodegenConfig = true;
+}
+
 module.exports = {
   project: {
     android: {
@@ -24,9 +44,22 @@ module.exports = {
       sourceDir: './example/windows',
       solutionFile: 'PickerExample.sln',
       project: {
-        projectFile: 'PickerExample/PickerExample.vcxproj'
-      }
-    }
+        projectFile: 'PickerExample/PickerExample.vcxproj',
+      },
+    },
+  },
+  dependency: {
+    platforms: {
+      android: supportsCodegenConfig
+        ? {
+            componentDescriptors: [
+              'RNCAndroidDialogPickerComponentDescriptor',
+              'RNCAndroidDropdownPickerComponentDescriptor',
+            ],
+            cmakeListsPath: '../android/src/main/jni/CMakeLists.txt',
+          }
+        : {},
+    },
   },
 };
 
